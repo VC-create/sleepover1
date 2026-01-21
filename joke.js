3b898a6f70fe9da35bcbeba197671c2bdf405567
VC-create <vedika.ar.chaudhari@gmail.com>
2026-01-21 04:15:57 +0000

    joke setup and punchline working, will add time delay and styling, and filter by categories as well

--- joke.js
+++ joke.js
@@ -2,8 +2,17 @@ document.addEventListener("DOMContentLoaded", showJoke)
 async function showJoke(){
     const jokeapi_call = await fetch("https://official-joke-api.appspot.com/random_joke")
     const jokeapi_json = await jokeapi_call.json()
+    
+    //this gets the setup of the joke because it's .setup
     const jokeSetup = jokeapi_json.setup
     const jokePunchline = jokeapi_json.punchline
-    const joke = document.getElementById("joke")
+    
+    //have a variable that represents the jokesetup, called joke
+    //change the text of that element to have the jokesetup
+    const joke = document.getElementById("jokeSetup")
     joke.innerText = jokeSetup
+
+    const answer = document.getElementById("jokePunchline")
+    answer.innerText = jokePunchline
+
 }
