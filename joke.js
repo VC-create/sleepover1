d9c92ee89470130ee299a5f818bb2bfc446a53da
VC-create <vedika.ar.chaudhari@gmail.com>
2026-01-21 04:12:30 +0000

    fixed the setup!

--- joke.js
+++ joke.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", showJoke)
 async function showJoke(){
     const jokeapi_call = await fetch("https://official-joke-api.appspot.com/random_joke")
     const jokeapi_json = await jokeapi_call.json()
+    const jokeSetup = jokeapi_json.setup
+    const jokePunchline = jokeapi_json.punchline
     const joke = document.getElementById("joke")
-    joke.value = jokeapi_json
+    joke.innerText = jokeSetup
 }
