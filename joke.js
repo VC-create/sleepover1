5bdfe0806518c863c4e7759beec7f1470a7a684c
VC-create <vedika.ar.chaudhari@gmail.com>
2026-01-21 04:18:53 +0000

    added some comments

--- joke.js
+++ joke.js
@@ -1,10 +1,17 @@
+//call the function when the page loads
 document.addEventListener("DOMContentLoaded", showJoke)
+
+//get the joke from the api and need to turn the raw data into json
+//need to turn it into json because you can't work with it in its raw form
 async function showJoke(){
     const jokeapi_call = await fetch("https://official-joke-api.appspot.com/random_joke")
     const jokeapi_json = await jokeapi_call.json()
     
+    //when you go the api, you can see the categories of setup and punchline
+
     //this gets the setup of the joke because it's .setup
     const jokeSetup = jokeapi_json.setup
+    //this gets the punchline because it's .punchline
     const jokePunchline = jokeapi_json.punchline
     
     //have a variable that represents the jokesetup, called joke
@@ -12,6 +19,8 @@ async function showJoke(){
     const joke = document.getElementById("jokeSetup")
     joke.innerText = jokeSetup
 
+    //have a variable that represents the jokepunchline, called answer
+    //change the text of that element to have the jokepunchline
     const answer = document.getElementById("jokePunchline")
     answer.innerText = jokePunchline
 
